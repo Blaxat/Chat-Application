ae3ead1fac413fd551681d49b5a798fb1bcdd0ed
Blaxat <136213598+Blaxat@users.noreply.github.com>
2024-07-24 22:43:53 +0530

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,6 +1,29 @@
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
+import Redis from 'ioredis';
+
+const pub = new Redis({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    username: process.env.REDIS_USERNAME,
+    password: process.env.REDIS_PASSWORD
+});
+
+const sub = new Redis({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    username: process.env.REDIS_USERNAME,
+    password: process.env.REDIS_PASSWORD
+});
+
+pub.on('connect', ( ) => {
+    console.log('Publisher');
+});
+
+sub.on('connect', ( ) => {
+    console.log('Publisher');
+});
 
 const app = express();
 
@@ -18,21 +41,50 @@ export const getReceiverSocketId = (receiverId) => {
 
 const userSocketMap = {}; // {userId: socketId}
 
-io.on("connection", (socket) => {
+io.on("connection", async (socket) => {
 	console.log("a user connected", socket.id);
 
 	const userId = socket.handshake.query.userId;
 	if (userId != "undefined") userSocketMap[userId] = socket.id;
 
-	// io.emit() is used to send events to all the connected clients
-	io.emit("getOnlineUsers", Object.keys(userSocketMap));
+	await pub.publish("onlineusers", JSON.stringify(Object.keys(userSocketMap)));
 
 	// socket.on() is used to listen to the events. can be used both on client and server side
-	socket.on("disconnect", () => {
+	socket.on("disconnect", async () => {
 		console.log("user disconnected", socket.id);
 		delete userSocketMap[userId];
-		io.emit("getOnlineUsers", Object.keys(userSocketMap));
+		await pub.publish("onlineusers", JSON.stringify(Object.keys(userSocketMap)));
 	});
 });
 
-export { app, io, server };
+sub.subscribe("onlineusers", (err, count) => {
+    if (err) {
+        console.error('Failed to subscribe:', err);
+    } else {
+        console.log(`Subscribed successfully! Listening for messages on 'onlineusers' channel...`);
+    }
+});
+
+sub.on('message', (channel, message) => {
+	io.emit("getOnlineUsers", message);
+    console.log(`Received message from channel '${channel}':`, message);
+});
+
+sub.subscribe('newMessage', (err, count) => {
+	if (err) {
+		console.error('Failed to subscribe to newMessage channel:', err);
+	} else {
+		console.log(`Subscribed to newMessage channel. Listening for messages...`);
+	}
+});
+
+sub.on('message', (channel, message) => {
+	if (channel === 'newMessage') {
+		const { message: newMessage, receiverSocketId } = JSON.parse(message);
+		console.log('Received new message from Redis:', newMessage);
+		console.log('Receiver Socket ID:', receiverSocketId);
+		io.to(receiverSocketId).emit("newMessage", newMessage);
+	}
+});
+
+export { app, io, server, pub, sub };
