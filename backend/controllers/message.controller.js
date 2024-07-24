9e3b339df8e680fa5cda067d789cdca41f60a844
Blaxat <akshaypandey047@gmail.com>
2024-07-24 21:43:33 +0530

    commit

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,6 +1,6 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
-import { getReceiverSocketId, io } from "../socket/socket.js";
+import { getReceiverSocketId, io, pub, sub } from "../socket/socket.js";
 
 export const sendMessage = async (req, res) => {
 	try {
@@ -28,17 +28,14 @@ export const sendMessage = async (req, res) => {
 			conversation.messages.push(newMessage._id);
 		}
 
-		// await conversation.save();
-		// await newMessage.save();
-
-		// this will run in parallel
 		await Promise.all([conversation.save(), newMessage.save()]);
 
-		// SOCKET IO FUNCTIONALITY WILL GO HERE
 		const receiverSocketId = getReceiverSocketId(receiverId);
 		if (receiverSocketId) {
-			// io.to(<socket_id>).emit() used to send events to specific client
-			io.to(receiverSocketId).emit("newMessage", newMessage);
+			pub.publish('newMessage', JSON.stringify({
+				message: newMessage,
+				receiverSocketId: getReceiverSocketId(receiverId)
+			}));
 		}
 
 		res.status(201).json(newMessage);
